969a02f49ea1bfbfcc420a8d03c64eb395d1c005
Darkstek <Darkstek@gmail.com>
2026-04-16 18:40:01 +0200

    hope fix

--- backend/models/Gateway.js
+++ backend/models/Gateway.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
- 
+
 const gatewaySchema = new mongoose.Schema({
   gatewayId: {
     type: String,
@@ -22,6 +22,18 @@ const gatewaySchema = new mongoose.Schema({
     type: Date,
     default: Date.now,
   },
+  tempMin: {
+    type: Number,
+    default: 20,
+  },
+  tempMax: {
+    type: Number,
+    default: 25,
+  },
+  prevWasAlert: {
+    type: Boolean,
+    default: false,
+  },
 });
- 
+
 module.exports = mongoose.model("Gateway", gatewaySchema);
